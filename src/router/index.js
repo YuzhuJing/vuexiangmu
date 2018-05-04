ea681ad7aa464cbc78a841701357371e5e75cffa
YuzhuJing <459761013@qq.com>
2018-05-04 19:36:23 +0800

    项目的所有页面已经布置好了，可以开发了

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,75 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+//引入商品相关的组件
+import Goodslist from '@/components/goods/list'
+import Goodsdetail from '@/components/goods/detail'
+
+//引入新闻相关的组件
+import Newsdetail from '@/components/news/detail'
+import Newslist from '@/components/news/list'
+
+//引入图片相关额组件
+import Picslist from '@/components/pics/list'
+import Picsdetail from '@/components/pics/detail'
+
+//引入tabbar相关的组件
+import Home from '@/components/tabbar/home'
+import Member from '@/components/tabbar/member'
+import Cart from '@/components/tabbar/cart'
+import Search from '@/components/tabbar/search'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-   
+    //商品
+    {
+      path:"/goods",
+      component:Goodslist
+    },
+    {
+      path:"/goods/detail",
+      component:Goodsdetail
+    },
+    //新闻
+    {
+      path:"/news",
+      component:Newslist
+    },
+    {
+      path:"/news/detail:id",
+      component:Newsdetail
+    },
+    //图片
+    {
+      path:"/pics",
+      component:Picslist
+    },
+    {
+      path:"/pics/detail",
+      component:Picsdetail
+    },
+    //tabbar
+    {
+      path:"/",
+      redirect:"/home"
+    },
+    {
+      path:"/home",
+      redirect:Home
+    },
+    {
+      path:"/memeber",
+      redirect:Member
+    },
+    {
+      path:"/cart",
+      redirect:Cart
+    },
+    {
+      path:"/search",
+      redirect:Search
+    }
   ]
 })
