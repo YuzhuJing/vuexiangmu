dbdfd4f27e1de4866d18e177a0500f78ec9cbd4c
YuzhuJing <459761013@qq.com>
2018-05-05 21:22:00 +0800

    新闻列表完成

--- src/components/news/list.vue
+++ src/components/news/list.vue
@@ -1,30 +1,13 @@
 <template>
     <div class="page">
       <ul class="mui-table-view mui-table-view-chevron">
-				<li class="mui-table-view-cell mui-media">
+
+				<li class="mui-table-view-cell mui-media" v-for="(v,i) in newslist" :key="i">
 					<a class="mui-navigate-right">
-						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
-						<div class="mui-media-body">
-							CBD
-							<p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a class='mui-navigate-right' href="javascript:;">
-						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
-						<div class="mui-media-body">
-							远眺
-							<p class='mui-ellipsis'>静静的看这个世界，最后终于疯了</p>
-						</div>
-					</a>
-				</li>
-				<li class="mui-table-view-cell mui-media">
-					<a class="mui-navigate-right">
-						<img class="mui-media-object mui-pull-left" src="../../assets/images/menu1.png">
-						<div class="mui-media-body">
-							幸福
-							<p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+						<img class="mui-media-object mui-pull-left" :src="v.img_url">
+						<div class="mui-media-body mui-ellipsis">
+							{{v.title}}
+							<p class='mui-ellipsis'>{{v.zhaiyao}}</p>
 						</div>
 					</a>
 				</li>
@@ -33,8 +16,23 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  
+  data(){
+    return{
+      newslist:[]
+    }
+  },
+  created(){
+    axios({
+      url:"http://www.escook.cn:3000/api/getnewslist"
+    }).then(res => {
+      if(res.data.status == 0){
+        this.newslist = res.data.message
+      }
+    })
+  }
 }
 </script>
 
